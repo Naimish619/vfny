a8712e828d00eb653d645b72a95b379c968e07a7
yatrikdesai <yatrik.desai3729@gmail.com>
2016-09-16 12:40:47 +0530

    update file added

---
+++ WebApplication1cachingandado.net/WebApplication1cachingandado.net/WebForm1.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+namespace WebApplication1cachingandado.net
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            String connstring = "Data Source=CELAB5;Integrated Security=True";
+            SqlConnection conn = new SqlConnection(connstring);
+            conn.Open();
+            String sql = "Update student set marks=@marks where sti_id=@id";
+            SqlCommand cmd = new SqlCommand(sql, conn);
+            cmd.Parameters.AddWithValue("@id", TextBox1.Text);
+            cmd.Parameters.AddWithValue("@marks", TextBox2.Text);
+            int added;
+
+
+            added = cmd.ExecuteNonQuery();
+
+
+
+            Label1.Text = added.ToString() + "record updated";
+        }
+    }
+}
